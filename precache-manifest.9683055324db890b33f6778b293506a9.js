20f33e6e49a914752d6c6ed6cee0e62a1122b734
Tosin <seanamos98@gmail.com>
2020-03-06 01:14:35 +0100

    Updates

---
+++ precache-manifest.9683055324db890b33f6778b293506a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4400ef24c06eef0f9fdcd94a78ef21fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02e36fa86d1d4f933445",
+    "url": "/static/css/main.5938617f.chunk.css"
+  },
+  {
+    "revision": "2386666e219945f4b1cc",
+    "url": "/static/js/2.2e805faf.chunk.js"
+  },
+  {
+    "revision": "e5fdd3c10a39c54f9723efcb1214ee6a",
+    "url": "/static/js/2.2e805faf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "02e36fa86d1d4f933445",
+    "url": "/static/js/main.37816dc5.chunk.js"
+  },
+  {
+    "revision": "c516647b492b96aa2e76",
+    "url": "/static/js/runtime-main.80e2530a.js"
+  }
+]);
